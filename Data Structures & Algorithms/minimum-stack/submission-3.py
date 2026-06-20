c4569a7c8fcb792d0360a278ba6b95df71cca9db
salt-vik <satvik0401@gmail.com>
2026-06-20 17:33:39 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,18 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[[1,int(1e20)]]
+
+    def push(self, val: int) -> None:
+            self.stack.append([val,min(self.stack[-1][1],val)])
+            
+        
+    def pop(self) -> None:
+         self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
